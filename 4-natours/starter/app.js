768517656ee8d3a20b3a838b5096c441920ba2a0
chei7tain <fredrickokwuobi@gmail.com>
2021-12-31 22:56:52 +0100

    implemented post request

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
+
 const fs = require('fs');
+const app = express();
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
 );
-const app = express();
 
+// get all tours
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -13,6 +16,15 @@ app.get('/api/v1/tours', (req, res) => {
     data: {
       tours,
     },
+  });
+});
+
+// post a new tour
+app.post('/api/v1/tours', (req, res) => {
+  let newTour = req.body;
+  console.log(newTour);
+  res.status(201).json({
+    status: 'success',
   });
 });
 const port = 3000;
