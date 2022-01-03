50a7faaf20c57dd544308bbbae3984fb96204b60
chei7tain <fredrickokwuobi@gmail.com>
2022-01-03 22:06:00 +0100

    add enviromental variables, installed dotenv package to allow access to the config.env file holding the enviromental variables

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -5,9 +5,11 @@ const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+
+
 app.use(express.json());
 app.use(morgan('dev'));
-
+app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
